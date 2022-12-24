c9307c100b36a286a7a90ed96ee5cc8eddec50c3
EktaMerai <ektamerai83@gmail.com>
2022-12-25 01:20:41 +0530

    React chapter1 react elements
    Trying render element with different value

---
+++ Session-1/code/App.js
@@ -0,0 +1,23 @@
+const heading = React.createElement("h1", { id: "Mainhdg" }, "Hello All Dev!");
+
+const heading_1 = React.createElement(
+  "h1",
+  { style: { color: "#4d53af"}, id: "h1tag" },
+  "Ekta here"
+);
+
+const heading_2 = React.createElement(
+  "h2",
+  { style: { color: "#583a0b" }, id: "key" },
+  "React new bee!!!"
+);
+
+const container = React.createElement("div", { id: "container" }, [
+  heading,
+  heading_1,
+  heading_2,
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);
